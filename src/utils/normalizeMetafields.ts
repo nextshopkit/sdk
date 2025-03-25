1a18c9f6b7864dd26d822acf7cfd2c2dc63920ca
NEDDL <mustafasaitonal@outlook.com>
2025-03-25 21:13:23 +0100

    put metafields nested

--- src/utils/normalizeMetafields.ts
+++ src/utils/normalizeMetafields.ts
@@ -2,11 +2,21 @@ type Metafield = { key: string; value: string };
 
 export function normalizeMetafields(
   metafields: (Metafield | null)[]
-): Record<string, string> {
-  return metafields.reduce((acc, field) => {
-    if (field?.key) {
-      acc[field.key] = field.value;
+): Record<string, any> {
+  const result: Record<string, any> = {};
+
+  for (const field of metafields) {
+    if (!field?.key) continue;
+    const [namespace, key] = field.key.includes(".")
+      ? field.key.split(".")
+      : ["global", field.key];
+
+    if (!result[namespace]) {
+      result[namespace] = {};
     }
-    return acc;
-  }, {} as Record<string, string>);
+
+    result[namespace][key] = field.value;
+  }
+
+  return result;
 }
